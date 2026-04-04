d2345e555b3e8add59ad014634d0e26ed6cee764
Yukti Gupta <yuktigupta95@gmail.com>
2026-04-04 18:18:16 +0530

    feat: connect API routes to Express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config()
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("App is running")
-})
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
 
 const PORT = 5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
